2c1124433d3641eae1f168e4d5d98e1f03c8f9fd
Anthony Hatch <anthonyhatch@Anthonys-MacBook-Pro.local>
2024-08-12 10:25:59 -0700

    adding rounded corners

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -16,7 +16,7 @@
         <div class="grid max-w-xl bg-white border rounded-md items-center pb-5">
           <h1 class="row-auto text-2xl text-center m-4 capitalize text-grey-700" >{{title}}</h1>
           <a :href=link_url target="_blank" class="row-auto items-center justify-self-center rounded">
-            <img class=" h-24 font-semibold mb-1.5 items-center" :src="image_url" alt={{title}}>  </img>
+            <img class=" h-24 font-semibold mb-1.5 items-center rounded" :src="image_url" alt={{title}}>  </img>
           </a>
         </div>
         <div>
