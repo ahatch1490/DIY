5431c5fa9cecd337a63df9dc8005f7f7fcc540eb
Anthony Hatch <ahatch1490@gmail.com>
2024-08-31 11:39:42 -0700

    adding base full detail view

--- src/components/GridViewDetail.vue
+++ src/components/GridViewDetail.vue
@@ -1,30 +1,27 @@
 <script setup lang="ts">
 import {GridItem} from "../types/GridItem.ts";
-import {ref} from "vue";
-import {Nullable} from "../types/Nullable.ts";
-let props = defineProps({ items:
+import {PropType} from "vue";
+import Stars from "./Stars.vue";
+defineProps({ selected:
       {
-        type: Array<GridItem>,
-        default: [],
+        type: Object as PropType<GridItem>,
+        default: null,
       }
 });
-let selected = ref<Nullable<GridItem>>(null)
 
-const selectionHandler = (gridItem: GridItem) => {
-  selected.value = gridItem;
-};
-selectionHandler(props.items[0]);
 </script>
 
 <template>
-  <div v-if="selected" class="max-w-xl border rounded-md shadow m-3">
     <div class="grid h-50 place-items-center m-3">
       <div class="color">
         <h1 class="text-4xl text-gray-700 text-center m-4 capitalize " >{{selected.title}}</h1>
         <img class=" h-50 font-semibold mb-1.5 border rounded border-0 " :src="selected.image_url" alt="{{selected.title}}" />
       </div>
+      <Stars v-if="selected.stars" :stars="selected.stars"></Stars>
     </div>
     <div class="text-sm rounded p-2.5 pb-2 " >{{selected.description}}</div>
+  <div>
+
   </div>
 </template>
 
